48e75d152536351a118abeab112ec65d8fb408f5
Ramiro Rivera <ramarivera@gmail.com>
2017-03-27 12:01:38 -0300

    fixed var not expanding

--- travis-ci/docker_build.sh
+++ travis-ci/docker_build.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+template_file="./templates/${CI_SO}"
 tag="${CI_SO}${CI_SO_VER}_jdk1.${CI_JAVA_MAJOR}_py${CI_PY_VER}"
 dockerfile_folder="./Dockerfiles/${CI_SO}${CI_SO_VER}/jdk1.${CI_JAVA_MAJOR}/py${CI_PY_VER}"
 dockerfile_path="${dockerfile_folder}/Dockerfile"
+
 mkdir -p $dockerfile_folder
-cp "./templates/${CI_SO}" dockerfile_path
+cp $template_file $dockerfile_path
 
 ci_env_vars=`env | awk 'match($0, /(CI_.*)=/) {print substr($0, RSTART, RLENGTH-1)}'`
 
@@ -15,10 +17,6 @@ done
 echo catting docker file path
 cat $dockerfile_path
 echo catted docker file path
-echo ci env vars
-echo $ci_env_vars
-echo ci env vars echoed
-
 
 docker build "${CI_IMAGE_NAME}:${tag}" $dockerfile_path
 
