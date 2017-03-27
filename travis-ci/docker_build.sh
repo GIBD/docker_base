d349e228836c77b162b892cd318cd8a436c2a825
Ramiro Rivera <ramarivera@gmail.com>
2017-03-27 13:03:29 -0300

    cat template before modif

--- travis-ci/docker_build.sh
+++ travis-ci/docker_build.sh
@@ -7,14 +7,13 @@ dockerfile_path="${dockerfile_folder}/Dockerfile"
 
 mkdir -p $dockerfile_folder
 cp $template_file $dockerfile_path
+cat $dockerfile_path
 
 ci_env_vars=`env | awk 'match($0, /(CI_.*)=/) {print substr($0, RSTART, RLENGTH-1)}'`
 
 for var_name in $ci_env_vars; do
     sed -e "s/:${var_name}:/${!var_name}/g" $dockerfile_path > $dockerfile_path
 done
-
-cat $template_file
 
 docker build --tag "${CI_IMAGE_NAME}:${tag}" $dockerfile_folder
 
