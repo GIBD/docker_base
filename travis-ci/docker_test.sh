8dc89ad3260f3b6637d1fad72ec00eaef448e961
Ramiro Rivera <ramarivera@gmail.com>
2017-03-27 16:45:51 -0300

    better tests

--- travis-ci/docker_test.sh
+++ travis-ci/docker_test.sh
@@ -6,6 +6,10 @@ message="Hello World"
 image_tag="${CI_SO}${CI_SO_VER}_jdk1.${CI_JAVA_MAJOR}_py${CI_PY_VER}"
 image_name="base:${image_tag}"
 
-docker run --name ${image_tag} ${image_name} echo ${message}
+echo_result = $(docker run --name ${image_tag} ${image_name} echo ${message})
 
-docker ps --all --filter 'exited=0' --filter "image=${image_name}" | grep -v "CONTAINER ID"
+if [ $message != $echo_result ]; then   
+  exit 1
+fi
+
+docker ps --all --filter 'exited=0' --filter "ancestor=${image_name}" | grep -v "CONTAINER ID"
