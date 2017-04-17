9eaa334c6c12095725b16f78bb0e230ccd94c443
Ramiro Rivera <ramarivera@gmail.com>
2017-04-17 20:20:58 -0300

    try again

--- travis-ci/docker_build.sh
+++ travis-ci/docker_build.sh
@@ -6,8 +6,8 @@ dockerfile_folder="./Dockerfiles/${CI_SO}${CI_SO_VER}/jdk1.${CI_JAVA_MAJOR}/py${
 dockerfile_path="${dockerfile_folder}/Dockerfile"
 
 mkdir -p $dockerfile_folder
-cp $template_folder/*.* ${dockerfile_folder}
-cp ./source/common/*.* ${dockerfile_folder}
+cp "./source/${CI_SO}/*.*" ${dockerfile_folder}
+cp "./source/common/*.*" ${dockerfile_folder}
 
 ci_env_vars=`env | awk 'match($0, /(CI_.*)=/) {print substr($0, RSTART, RLENGTH-1)}'`
 
