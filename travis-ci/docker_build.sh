6a20cbe4e17c1b741a5d2e04d33eb9b2857e2842
Ramiro Rivera <ramarivera@gmail.com>
2017-04-18 03:32:52 -0300

    remove dockerfilee

--- travis-ci/docker_build.sh
+++ travis-ci/docker_build.sh
@@ -13,8 +13,10 @@ cp "./source/common"/* "${dockerfile_folder}/"
 ci_env_vars=`env | awk 'match($0, /(CI_.*)=/) {print substr($0, RSTART, RLENGTH-1)}'`
 
 for var_name in $ci_env_vars; do
-    sed -ie "s/:${var_name}:/${!var_name}/g" $dockerfile_path
+    sed -i.bak "s/:${var_name}:/${!var_name}/g" $dockerfile_path
 done
+
+rm -rf "${dockerfile_path}.bak"
 
 docker build --tag "${CI_IMAGE_NAME}:${tag}" $dockerfile_folder
 
